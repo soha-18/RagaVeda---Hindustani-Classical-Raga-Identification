ff2721a52c02b8bc3bd428b39fb0c7e16568988b
Sohini Maji <98654416+soha-18@users.noreply.github.com>
2025-04-22 19:00:27 +0530

    Update dataframe headers

--- feature_extraction.py
+++ feature_extraction.py
@@ -57,8 +57,8 @@ feature_df = pd.DataFrame(features)
 ragas_df = pd.DataFrame({'genre': ragas})
 dataset = pd.concat([feature_df, ragas_df], axis=1)
 prefix = "mfcc_"
-new_columns = [prefix + str(col) for col in dataset.columns]
-dataset.columns = new_columns
+new_columns = [prefix + str(col) for col in dataset.columns[:-1]]
+dataset.columns = new_columns + [dataset.columns[-1]]
 
 print("\nDataset created successfully!")
 print(dataset.head())
