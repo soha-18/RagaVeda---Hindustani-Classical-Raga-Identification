e6ff700922752c7459983496f58d14b45fcce63f
Sohini Maji <98654416+soha-18@users.noreply.github.com>
2025-05-06 19:22:58 +0530

    Adding audio augmentation to create training dataset

--- feature_extraction.py
+++ feature_extraction.py
@@ -9,24 +9,23 @@ import matplotlib.pyplot as plt
 ragas = []
 features = []
 
-def audio_augmentation(audio_path, augmentation_type='time_stretch', factor=None):
-    y, sr = librosa.load(audio_path, duration=30)
+def audio_augmentation(audio_file, sr, augmentation_type='time_stretch', factor=None):
     if augmentation_type == 'time_stretch':
         if factor is None:
             factor = random.uniform(0.8, 1.2)
-        y_stretched = librosa.effects.time_stretch(y, rate=factor)
-        return y_stretched, sr
+        y_stretched = librosa.effects.time_stretch(audio_file, rate=factor)
+        return y_stretched
     elif augmentation_type == 'pitch_shift':
         if factor is None:
             factor = random.uniform(-2, 2)
-        y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=factor)
-        return y_shifted, sr
+        y_shifted = librosa.effects.pitch_shift(audio_file, sr=sr, n_steps=factor)
+        return y_shifted
     elif augmentation_type == 'add_noise':
-        noise = 0.005 * np.random.randn(len(y))
-        y_noisy = y + noise
-        return y_noisy, sr
+        noise = 0.005 * np.random.randn(len(audio_file))
+        y_noisy = audio_file + noise
+        return y_noisy
     else:
-        return y, sr
+        return audio_file
   
 def create_spectrogram(file, n_fft, hop):
     try:
@@ -70,9 +69,10 @@ for ragas_folder in os.listdir("Datasets/"):
             if filename.endswith(".wav") or filename.endswith(".mp3"):
                 audio_path = os.path.join(ragas_path, filename)
                 try:
-                    y_stretched, sr = audio_augmentation(audio_path, augmentation_type='time_stretch')
-                    y_shifted, sr = audio_augmentation(audio_path, augmentation_type='pitch_shift')
-                    aug_audio, sr = audio_augmentation(audio_path, augmentation_type='add_noise')
+                    y, sr = librosa.load(audio_path, duration=30)
+                    y_stretched = audio_augmentation(y, sr, augmentation_type='time_stretch')
+                    y_shifted = audio_augmentation(y_stretched, sr, augmentation_type='pitch_shift')
+                    aug_audio = audio_augmentation(y_shifted, sr, augmentation_type='add_noise')
                     feature_vector = extract_mfcc_feature_vector(aug_audio, sr)
                     features.append(feature_vector)
                     ragas.append(ragas_folder)
@@ -94,4 +94,3 @@ print("\nDataset created successfully!")
 
 # Convert dataset to csv
 dataset.to_csv("audio_features_dataset.csv", index=False)
-    
