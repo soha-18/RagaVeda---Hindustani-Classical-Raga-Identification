0df1202bb2f561259d295bfc6a3269e9206178a2
Sohini Maji <sohini.maji@tuhh.de>
2025-05-13 12:52:47 +0200

    updated

--- mel_spectogram_datasets.py
+++ mel_spectogram_datasets.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import librosa
 import numpy as np
@@ -35,41 +34,3 @@ for ragas_folder in os.listdir("Datasets/"):
 feature_df = pd.DataFrame({'Mel_Features':mel_features})
 ragas_df = pd.DataFrame({'Ragas': ragas})
 mel_dataset = pd.concat([feature_df, ragas_df], axis=1)
-=======
-import os
-import librosa
-import numpy as np
-import pandas as pd
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import matplotlib.pyplot as plt
-
-mel_features = []
-ragas = []
-
-def extract_features_mel(file, sr, n_mels=128, max_pad_len=174):
-    mel_spec = librosa.feature.melspectrogram(y=file, sr=sr, n_mels=n_mels)
-    mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
-    mel_spec_db = mel_spec_db.T
-    mel_spec_db_padded = pad_sequences([mel_spec_db], padding='post', maxlen=max_pad_len, dtype='float32')[0]
-
-    return mel_spec_db_padded
-
-for ragas_folder in os.listdir("Datasets/"):
-    ragas_path = os.path.join("Datasets/", ragas_folder)
-    if os.path.isdir(ragas_path):
-        for filename in os.listdir(ragas_path):
-            if filename.endswith(".wav") or filename.endswith(".mp3"):
-                audio_path = os.path.join(ragas_path, filename)
-                try:
-                    y, sr = librosa.load(audio_path, duration=30)
-                    feature_vector = extract_features_mel(y, sr)
-                    mel_features.append(feature_vector)
-                    ragas.append(ragas_folder)
-
-                except Exception as e:
-                    print(f"Error processing {filename}: {e}")
-
-feature_df = pd.DataFrame({'Mel_Features':mel_features})
-ragas_df = pd.DataFrame({'Ragas': ragas})
-mel_dataset = pd.concat([feature_df, ragas_df], axis=1)
->>>>>>> 847f00ac4ef715b3adb8de6f9ac0ce8133bdee64
