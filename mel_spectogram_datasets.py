253ebb20d30e95bcd4bbb1d0caafeb2364d7ef3f
Sohini Maji <98654416+soha-18@users.noreply.github.com>
2025-05-25 22:26:35 +0200

    Updating dataset structure

--- mel_spectogram_datasets.py
+++ mel_spectogram_datasets.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 
 mel_features = []
 ragas = []
+mel_dataset = []
 
 def extract_features_mel(file, sr, n_mels=128, max_pad_len=174):
     mel_spec = librosa.feature.melspectrogram(y=file, sr=sr, n_mels=n_mels)
     mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
     mel_spec_db = mel_spec_db.T
-    mel_spec_db_padded = pad_sequences([mel_spec_db], padding='post', maxlen=max_pad_len, dtype='float64')[0]
+    mel_spec_db_padded = pad_sequences([mel_spec_db], padding='post', maxlen=max_pad_len, dtype='float32')[0]
 
     return mel_spec_db_padded
 
@@ -25,15 +26,17 @@ for ragas_folder in os.listdir("Datasets/"):
                 try:
                     y, sr = librosa.load(audio_path, duration=30)
                     feature_vector = extract_features_mel(y, sr)
-                    mel_features.append(feature_vector)
-                    ragas.append(ragas_folder)
+                    #mel_features.append(feature_vector)
+                    #ragas.append(ragas_folder)
+                    mel_features.append([feature_vector, ragas_folder])
 
                 except Exception as e:
                     print(f"Error processing {filename}: {e}")
 
-feature_df = pd.DataFrame({'Mel_Features':mel_features})
-ragas_df = pd.DataFrame({'Ragas': ragas})
-mel_dataset = pd.concat([feature_df, ragas_df], axis=1)
+#feature_df = pd.DataFrame({'Mel_Features':mel_features})
+#ragas_df = pd.DataFrame({'Ragas': ragas})
+#mel_dataset = pd.concat([feature_df, ragas_df], axis=1)
+mel_dataset = pd.DataFrame(mel_features)
 
-print("\nDataset created successfully!")
 mel_dataset.to_csv("mel_features_dataset.csv", index=False)
+print("\nDataset created successfully!")
