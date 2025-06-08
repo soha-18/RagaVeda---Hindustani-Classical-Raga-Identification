fdd3ad7cfe8d3585b8b9be9fbf63a7ed4008a029
Sohini Maji <98654416+soha-18@users.noreply.github.com>
2025-06-08 14:39:41 +0200

    Generated test dataset of mel spectogram features

--- test_data_generation.py
+++ test_data_generation.py
@@ -27,32 +27,34 @@ for root, dirs, files in os.walk(audio_folder):
                     y_shifted = audio_augmentation(y_stretched, sr, augmentation_type='pitch_shift')
                     aug_audio = audio_augmentation(y_shifted, sr, augmentation_type='add_noise')
                     feature_vector = extract_mfcc_feature_vector(aug_audio, sr)
-                    #feature_vector = extract_mfcc_feature_vector(file_path)
+                    feature_vector_mel = extract_features_mel(y, sr)
                     test_features.append(feature_vector)
                     test_ragas.append(labels)
+                    mel_test_features.append([feature_vector_mel, labels])
                     #print(f"File Processed")
 
                 except Exception as e:
                     print(f"Error processing {file}: {e}")
 
 ## Convert the lists to a Pandas DataFrame
-test_feature_df = pd.DataFrame(test_features)
-test_ragas_df = pd.DataFrame({'Ragas': test_ragas})
-test_dataset = pd.concat([test_feature_df, test_ragas_df], axis=1)
-prefix = "mfcc_"
-new_columns = [prefix + str(col) for col in test_dataset.columns[:-1]]
-test_dataset.columns = new_columns + [test_dataset.columns[-1]]
-test_dataset['Ragas'] = test_dataset['Ragas'].str.replace('\d+', '', regex=True)
-test_dataset['Ragas'] = test_dataset['Ragas'].replace(['bhoop', 'bhoopali'], 'Bhoopali')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace(['DKanada', 'darbari'], 'Darbari')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace('bhairavi', 'Bhairavi')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace('bageshree', 'Bageshree')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace('asavari', 'Asavari')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace('sarang', 'Sarang')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace('malkauns', 'Malkauns')
-test_dataset['Ragas'] = test_dataset['Ragas'].replace('yaman', 'Yaman')
+test_mel_dataset = pd.DataFrame(mel_test_features, columns = ("Mel_Features", "Ragas"))
+test_mel_dataset['Ragas'] = test_mel_dataset['Ragas'].str.replace('\d+', '', regex=True)
+# test_feature_df = pd.DataFrame(test_features)
+# test_ragas_df = pd.DataFrame({'Ragas': test_ragas})
+# test_dataset = pd.concat([test_feature_df, test_ragas_df], axis=1)
+# prefix = "mfcc_"
+# new_columns = [prefix + str(col) for col in test_dataset.columns[:-1]]
+# test_dataset.columns = new_columns + [test_dataset.columns[-1]]
+# test_dataset['Ragas'] = test_dataset['Ragas'].str.replace('\d+', '', regex=True)
+# test_dataset['Ragas'] = test_dataset['Ragas'].replace(['bhoop', 'bhoopali'], 'Bhoopali')
+# test_dataset['Ragas'] = test_dataset['Ragas'].replace(['DKanada', 'darbari'], 'Darbari')
+# test_dataset['Ragas'] = test_dataset['Ragas'].str.capitalize()
+test_mel_dataset['Ragas'] = test_mel_dataset['Ragas'].str.capitalize()
+test_mel_dataset['Ragas'] = test_mel_dataset['Ragas'].replace('bhoop', 'Bhoopali')
+test_mel_dataset['Ragas'] = test_mel_dataset['Ragas'].replace('DKanada', 'Darbari')
 print("\nDataset created successfully!")
 #print(dataset.head())
 
 # Convert dataset to csv
-test_dataset.to_csv("test_dataset.csv", index=False)
+test_mel_dataset.to_csv("test_mel_dataset.csv", index=False)
+# test_dataset.to_csv("test_dataset.csv", index=False)
