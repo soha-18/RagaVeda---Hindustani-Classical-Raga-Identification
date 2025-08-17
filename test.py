1f35c02e9b72fda0e0e87977f4518a3496084ad4
Sohini Maji <sohinimaji18@gmail.com>
2025-08-17 13:58:18 +0200

    transforming mel dataset

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 import re
 
 # data = []
