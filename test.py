a91f627ecf32676ba00d77744726683f2e8eaaed
Sohini Maji <98654416+soha-18@users.noreply.github.com>
2025-08-20 19:31:13 +0200

    Removing npz file operations

--- test.py
+++ test.py
@@ -22,27 +22,4 @@ import re
 #         except ValueError:
 #             print(f"Skipping malformed line: {line}")
 
-# Load the .npz file
-data = np.load('mel_features.npz', allow_pickle=True)
 
-# Access the arrays by their key names
-X_loaded = data['arr1']
-y_loaded = data['arr2']
-
-# You can now use X_loaded and y_loaded for further preprocessing or model training
-print("Shape of loaded X:", X_loaded.shape)
-print("Shape of loaded y:", y_loaded.shape)
-
-data.close()
-print(X_loaded.dtype)
-X_loaded = np.vstack(X_loaded)
-X_loaded = X_loaded.astype(float)
-# scaler = StandardScaler()
-# X_scaled = scaler.fit_transform(X_loaded)
-# print("Shape of scaled X:", X_scaled.shape)
-# df = pd.DataFrame(data)
-# #print(df)
-# X = df['features'].values
-# print(X.shape)
-# print(X[23])
-# #print(type(X[1]))
