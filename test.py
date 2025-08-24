46df5f0e49afa5a0965f4548ec488394c07c1e27
Sohini Maji <sohinimaji18@gmail.com>
2025-08-24 15:45:01 +0200

    .

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ with open('mel_dataset.csv', 'r') as f:
         # Split the line by the comma
         parts = line.strip().split(',')
         features_str = parts[0].strip('[]')
-        #label = parts[1].strip()
 
         try:
             numbers = re.findall(r'-?\d+\.?\d*', features_str)
@@ -18,7 +17,7 @@ with open('mel_dataset.csv', 'r') as f:
             # Clean up the string to make it a valid list representation
             features_str = features_str.replace('...', '').replace('[', '').replace(']', '')
             features = np.fromstring(features_str, sep=' ')
-            data.append({'features': features})  #, 'label': label})
+            data.append({'features': features})
         except ValueError:
             print(f"Skipping malformed line: {line}")
 
